8c64566a8e1a9e3b447f0d8972898f7c25ecad2c
John Luke Bentley <johnny_bentley@yahoo.com.au>
2024-07-15 12:26:20 +1000

    httpsWrapper refactor to allow optional additional headers

--- httpWrapper.ts
+++ httpWrapper.ts
@@ -8,32 +8,37 @@ import AbortController from 'abort-controller'
 import * as Http from './http.js'
 export { DatabaseResponse } from './http.js'
 
-export async function postData(data: object, url: string): Promise<Http.DatabaseResponse> {
+export async function postData(data: object, url: string, headers?: Record<string, string>): Promise<Http.DatabaseResponse> {
+  const abortController = new AbortController();
+  let timeoutId: NodeJS.Timeout;
 
-  const abortController = new AbortController()
-  let timeoutId: NodeJS.Timeout
+  // Default headers
+  const defaultHeaders = { 'Content-Type': 'application/json' };
+
+  // Merge default headers with provided headers
+  const mergedHeaders = { ...defaultHeaders, ...headers };
 
   const spatialPromise = new Promise<Http.DatabaseResponse>((resolve, reject) => {
     Http.sendRequest(url, {
       method: 'POST',
       body: JSON.stringify(data),
-      headers: { 'Content-Type': 'application/json' },
+      headers: mergedHeaders,
       signal: abortController.signal,
     }).then((response: Http.DatabaseResponse) => {
-      console.log('Response from database: ', response)
-      timeoutId && clearTimeout(timeoutId)
-      resolve(response)
-    }, reject)
-  })
+      console.log('Response from database: ', response);
+      timeoutId && clearTimeout(timeoutId);
+      resolve(response);
+    }, reject);
+  });
 
   const timeoutPromise = new Promise<Http.DatabaseResponse>((resolve) => {
     timeoutId = setTimeout(() => {
-      console.log('Database timed out, returning 200')
-      abortController.abort()
-      const abortPacket: Http.DatabaseResponse = { body: "Aborted", statusCode: 400}
-      resolve(abortPacket)
-    }, 24000)
-  })
-  
-  return await Promise.race([spatialPromise, timeoutPromise])
+      console.log('Database timed out, returning 200');
+      abortController.abort();
+      const abortPacket: Http.DatabaseResponse = { body: "Aborted", statusCode: 400 };
+      resolve(abortPacket);
+    }, 24000);
+  });
+
+  return await Promise.race([spatialPromise, timeoutPromise]);
 }
